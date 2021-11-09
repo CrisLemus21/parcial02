3f2fee2687342c94bcda884e7f8d1b42548d4e4a
CrisLemus21 <87676318+CrisLemus21@users.noreply.github.com>
2021-11-09 02:05:13 -0600

    v1
    las 2 de la mañana, y valiendo por un dia que falte.

---
+++ parcial2/parcial2/Controllers/CajaController.cs
@@ -0,0 +1,48 @@
+﻿using parcial2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace parcial2.Controllers
+{
+    public class CajaController : Controller
+    {
+        // GET: Caja
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult indexV()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult noSirve()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult sirve()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult indexV(caja ca)
+        {
+            if(ca.caj % 5 == 0)
+            {
+                return Redirect("sirve");
+
+            }
+            else
+            {
+                return Redirect("noSirve");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ parcial2/parcial2/Models/caja.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace parcial2.Models
+{
+    public class caja
+    {
+        public int caj{ get; set; }
+    }
+}
